75c5d7cc1c10ffc85530c52a02fbe425d617546f
klaria bestDeal <klaria@bestdeal.com.br>
2023-07-28 18:03:17 -0300

    até a parte 08

--- jogo_de_adivinhação/main.py
+++ jogo_de_adivinhação/main.py
@@ -1,33 +1,14 @@
-print("Bem vindo no jogo de adivinhação")
+import forca
 
-numero_secreto = 42
-total_de_tentativas = 3
+print("*********************************************************************")
+print("************************** Escolha o seu jogo ***********************")
+print("*********************************************************************")
 
-for rodada in range (1,total_de_tentativas + 1):
-    print("Tentiva {} de {}".format(rodada,total_de_tentativas))
+print("(1) Força (2) Adivinhação")
 
-    chute_str = input("Digite o seu número:")
-    print("Você digitou:", chute_str)
+jogo = int(input())
 
-    # Conversão para inteiro leitura e sempre string
-    chute = int(chute_str)
-
-    if (chute < 1 or chute > 100):
-        print("Você deve digitar um número entre 1 e 100!")
-        continue
-
-    # declarando os criterios
-    acertou = chute == numero_secreto
-    maior = chute > numero_secreto
-    menor = chute < numero_secreto
-
-    if (acertou):
-        print("Você acertou")
-        break
-    else:
-        if (maior):
-            print("Seu chute foi maior que o número secreto", end="\n")
-        elif (menor):
-            print("Seu chute foi menor que o número secreto ", end="\n")
-
-print("Fim do jogo", end="\n")
+if(jogo==1):
+    print('beleza')
+else:
+    print('beleza')
