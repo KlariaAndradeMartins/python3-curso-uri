0e4c85ee21eba502e28384f3a0ada7bc20861d94
KlariaAndradeMartins <klariaandrade@gmail.com>
2023-08-08 22:46:23 -0300

    Relaxando fazendo exercicios no becrownd

---
+++ uri_judge/Iniciante /1012.py
@@ -0,0 +1,16 @@
+a,b,c = list(map(float,input().split()))
+
+#Triangulo 
+print(f'TRIANGULO: {0.5*a*c:.3f}')
+
+#circulo
+print(f"CIRCULO: {3.14159*c**2:.3f}")
+
+#Trapezio
+print(f"TRAPEZIO: {(a+b)*c/2:.3f}")
+
+#quadrado 
+print(f"QUADRADO: {b*b:.3f}")
+
+#retangulo
+print(f"RETANGULO: {a*b:.3f}")
